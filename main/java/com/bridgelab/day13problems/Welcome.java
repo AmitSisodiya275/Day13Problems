45702e016c3f6667e7c8fdbdb8491d8a07a906c3
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-10-04 19:41:20 +0530

    Added: a new welcome file which can print welcome message on the console.

---
+++ main/java/com/bridgelab/day13problems/Welcome.java
@@ -0,0 +1,10 @@
+package com.bridgelab.day13problems;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the Day 13 Problems of Generics.");
+	}
+
+}
